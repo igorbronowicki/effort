447838e142c61d399920b3168b94adbb2b5486c9
Igor Bronowicki <ibronovytskyi@ebay.com>
2013-07-21 11:08:28 +0300

    base html + base JavaScript

--- public/app.js
+++ public/app.js
@@ -1,1 +1,143 @@
-console.log("Hello world!");
+$(function(){
+
+
+/**
+ * Создание объекта нашего приложения (namespace).
+ */
+window.app = {
+    el: $("#app"),
+    view: {},
+    init: function() {
+        app.view.statistics.render();
+        app.view.nickname.render();
+    }
+};
+
+
+/**
+ * View для отрисовки экрана статистики.
+ */
+app.view.statistics = {
+    el: $("#statistics"),
+    template: $("#tpl-statistics").html(),
+    model: {},
+
+    init: function() {
+        // TODO: Навесить события? "click .destroy" : empty
+    },
+
+    render: function() {
+        $(this.el).html(Mustache.render(this.template, this.model));
+
+        return this;
+    },
+
+    empty: function() {
+        $(this.el).empty();
+    }
+};
+
+
+/**
+ * View для отрисовки экрана ввода имени.
+ */
+app.view.nickname = {
+    el: $("#nickname"),
+    template: $("#tpl-nickname").html(),
+    model: {},
+
+    init: function() {
+        // TODO: Навесить события? "click .destroy" : empty
+    },
+
+    render: function() {
+        $(this.el).html(Mustache.render(this.template, this.model));
+
+        return this;
+    },
+
+    empty: function() {
+        $(this.el).empty();
+    }
+};
+
+
+/**
+ * View для отрисовки экрана списка игр.
+ */
+app.view.games = {
+    el: $("#games"),
+    template: $("#tpl-games").html(),
+    model: {},
+
+    init: function() {
+        // TODO: Навесить события? "click .destroy" : empty
+    },
+
+    render: function() {
+        $(this.el).html(Mustache.render(this.template, this.model));
+
+        return this;
+    },
+
+    empty: function() {
+        $(this.el).empty();
+    }
+};
+
+
+/**
+ * View для отрисовки экрана деталей игры.
+ */
+app.view.details = {
+    el: $("#details"),
+    template: $("#tpl-details").html(),
+    model: {},
+
+    init: function() {
+        // TODO: Навесить события? "click .destroy" : empty
+    },
+
+    render: function() {
+        $(this.el).html(Mustache.render(this.template, this.model));
+
+        return this;
+    },
+
+    empty: function() {
+        $(this.el).empty();
+    }
+};
+
+
+/**
+ * View для отрисовки экрана игры.
+ */
+app.view.game = {
+    el: $("#game"),
+    template: $("#tpl-game").html(),
+    model: {},
+
+    init: function() {
+        // TODO: Навесить события? "click .destroy" : empty
+    },
+
+    render: function() {
+        $(this.el).html(Mustache.render(this.template, this.model));
+
+        return this;
+    },
+
+    empty: function() {
+        $(this.el).empty();
+    }
+};
+
+
+/**
+ * Инициализация нашего приложения.
+ */
+app.init();
+
+
+});
